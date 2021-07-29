5cca187b89389d3b2ca0d187c3b6d0d31182eaee
Wurm0009 <70657505+Wurm0009@users.noreply.github.com>
2021-07-29 14:17:58 +0300

    Delete bobmer2 directory

--- bobmer2/modules/update.py
+++
@@ -1,4 +0,0 @@
-import os, sys
-
-os.system("cd .. && rm -rf ptr0vbomberrrr2 && git clone https://github.com/Wurm0009/ptr0vbomberrrr2 && cd ptr0vbomberrrr2 && clear")
-print('обновление завершено!')
