4f9bca3f0557946b722f5e5e55f18072454de0e3
Wurm0009 <70657505+Wurm0009@users.noreply.github.com>
2021-07-29 14:18:39 +0300

    Add files via upload

---
+++ ptr0vbomberrrr2.py
@@ -0,0 +1,37 @@
+import requests, random, datetime, sys, time, os
+
+from termcolor import colored
+from modules import start
+
+def bomber():
+    os.system("cd bombers && python3 bomber.py")
+
+def creator():
+    print(start.creator)
+    input()
+    main()
+
+def update():
+    os.system("cd modules && python3 update.py")
+
+def exit():
+    os.system("clear")
+    print('Пока-Пока!...)')
+    sys.exit()
+
+def main():
+    os.system('clear')
+    print(logo.banner)
+    print(logo.menu)
+    num_menu = input("iisus> ")
+    if num_menu == "":
+       main()
+    if num_menu == "1":
+        bomber()
+    if num_menu == "2":
+        creator()
+    if num_menu == "3":
+        update()
+    if num_menu == "4":
+        exit()
+main()
